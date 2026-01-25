9d2b75c4b5f83fbc9d0d0f943e3d7ad4a3c38ac8
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-25 21:08:43 +0530

    Add method signature for absolute method

--- maths/Absolute.java
+++ maths/Absolute.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class Absolute {
     // Program to find absolute value of a given number
 
+    public static int absolute(int n) {
+        return 0;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a integer: ");
         int n = scanner.nextInt();
 
-        
+
         scanner.close();
     }
 }
