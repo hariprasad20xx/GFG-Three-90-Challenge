df76a2f062ee0c07c4fa73c307f404899728dfe3
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-25 21:05:24 +0530

    Create Scanner object

--- maths/Absolute.java
+++ maths/Absolute.java
@@ -4,6 +4,8 @@ public class Absolute {
     // Program to find absolute value of a given number
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        scanner.close();
     }
 }
