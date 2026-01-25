91505ef098b8b99bff14e1d4fa7fd6f5222617f8
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-25 21:10:50 +0530

    Add logic to find absolute value

--- maths/Absolute.java
+++ maths/Absolute.java
@@ -4,14 +4,14 @@ public class Absolute {
     // Program to find absolute value of a given number
 
     public static int absolute(int n) {
-        return 0;
+        return (n < 0) ? -1 * n : n;
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a integer: ");
         int n = scanner.nextInt();
-
 
         scanner.close();
     }
