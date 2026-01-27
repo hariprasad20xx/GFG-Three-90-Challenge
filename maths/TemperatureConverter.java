f9d5f5df2298f55f26c1b2cb5d59957b60a261c5
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-27 21:49:10 +0530

    Take user input

--- maths/TemperatureConverter.java
+++ maths/TemperatureConverter.java
@@ -10,6 +10,8 @@ public class TemperatureConverter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter temperature in celsius: ");
+        double temperature = scanner.nextDouble();
         scanner.close();
     }
 }
