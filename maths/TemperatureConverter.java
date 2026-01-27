0750ac9da15d09c2e13ac13faa41d51c722c4cca
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-27 21:43:47 +0530

    Add method signature for cToF method

--- maths/TemperatureConverter.java
+++ maths/TemperatureConverter.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class TemperatureConverter {
     // Program to convert temperature
     
+    public static double cToF(int C) {
+        return 0.0;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
