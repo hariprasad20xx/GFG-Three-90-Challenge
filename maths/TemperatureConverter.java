e9c840ac902d61db0c057cce2a164506d9e338dd
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-27 21:54:03 +0530

    Add temperature conversion code

--- maths/TemperatureConverter.java
+++ maths/TemperatureConverter.java
@@ -4,7 +4,7 @@ public class TemperatureConverter {
     // Program to convert temperature
     
     public static double cToF(double C) {
-        return 0.0;
+        return C * 1.8 + 32;
     }
 
     public static void main(String[] args) {
