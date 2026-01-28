c4081f70c0d5751f47c24d47fdcf37d09ae30bcb
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-28 22:40:57 +0530

    Create Scanner object

--- maths/QuadraticRoots.java
+++ maths/QuadraticRoots.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class QuadraticRoots {
     // Program to find roots of a quadratic equation
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        scanner.close();
     }
 }
