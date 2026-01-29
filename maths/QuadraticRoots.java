3b7ccacef6be0f8a22251ebd9c2f3bf24a5d9ac4
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-29 20:54:48 +0530

    Import ArrayList class

--- maths/QuadraticRoots.java
+++ maths/QuadraticRoots.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class QuadraticRoots {
     // Program to find roots of a quadratic equation
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
