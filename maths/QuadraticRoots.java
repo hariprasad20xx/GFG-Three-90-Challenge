fdb9a59b38a046313602ab6934ae714d254ac4c3
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-29 21:06:49 +0530

    Implement logic to calculate roots

--- maths/QuadraticRoots.java
+++ maths/QuadraticRoots.java
@@ -4,9 +4,20 @@ import java.util.Scanner;
 public class QuadraticRoots {
     // Program to find roots of a quadratic equation
     public static ArrayList<Integer> findRoots(int a, int b, int c) {
-        return new ArrayList<>();
+        int d = b * b - (4 * a * c);
+        
+        ArrayList<Integer> roots = new ArrayList<>();
+        
+        if (d >= 0) {
+            roots.add((int) Math.floor((-b + Math.sqrt(d)) / (2 * a)));
+            roots.add((int) Math.floor((-b - Math.sqrt(d)) / (2 * a)));
+        } else {
+            roots.add(-1);
+        }
+        
+        return roots;
     }
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
