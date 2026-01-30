d7655f0af0d34400b2df238ed25084ed2256ad69
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-30 12:43:11 +0530

    Add method signature for method factorial

--- maths/Factorial.java
+++ maths/Factorial.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 public class Factorial {
     // Program to find a factorial of a number
-
+    public static int factorial(int n) {
+        return 0;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a positive integer: ");
         int n = scanner.nextInt();
-        
+
         scanner.close();
     }
 }
