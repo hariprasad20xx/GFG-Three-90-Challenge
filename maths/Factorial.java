686ab323164c07c6e7aa5f5df9c9448ae00daaa2
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-30 12:42:20 +0530

    Take user input

--- maths/Factorial.java
+++ maths/Factorial.java
@@ -6,6 +6,9 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter a positive integer: ");
+        int n = scanner.nextInt();
+        
         scanner.close();
     }
 }
