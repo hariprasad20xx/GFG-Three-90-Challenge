9b5c0385d7cfc1b57c5edff68347be3f2a6fa979
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-31 18:48:47 +0530

    Create Scanner object

--- maths/DigitsInFactorial.java
+++ maths/DigitsInFactorial.java
@@ -4,6 +4,8 @@ public class DigitsInFactorial {
     // Program to find digits in a factorial
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        scanner.close();
     }
 }
