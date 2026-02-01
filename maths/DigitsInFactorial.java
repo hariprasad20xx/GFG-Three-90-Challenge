faf6bcd9b5542c19a2eaaf88129eaa3132116c74
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-01 21:49:17 +0530

    Add method signature for digitsInFactorial method

--- maths/DigitsInFactorial.java
+++ maths/DigitsInFactorial.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 
 public class DigitsInFactorial {
     // Program to find digits in a factorial
+    public static int digitsInFactorial(int n) {
+        return 0;
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter an integer: ");
         int n = scanner.nextInt();
-        
+
         scanner.close();
     }
 }
