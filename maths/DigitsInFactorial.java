0b4bb086fd5b66ea6102c347c8def2ed26bdcd73
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-01 21:52:52 +0530

    Implement algorithm to calculate digits in factorial

--- maths/DigitsInFactorial.java
+++ maths/DigitsInFactorial.java
@@ -3,7 +3,16 @@ import java.util.Scanner;
 public class DigitsInFactorial {
     // Program to find digits in a factorial
     public static int digitsInFactorial(int n) {
-        return 0;
+        int numOfDigits = 0;
+        double logSum = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            logSum = logSum + (Math.log10(i));
+        }
+        
+        numOfDigits = (int)Math.floor(logSum) + 1;
+        
+        return numOfDigits;
     }
 
     public static void main(String[] args) {
