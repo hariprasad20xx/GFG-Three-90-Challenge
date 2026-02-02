aeb230d7f420e1f515cc3c1a35fa2da68d136b12
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-02 21:39:32 +0530

    Import Scanner class

--- maths/PrimeNumber.java
+++ maths/PrimeNumber.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 public class PrimeNumber {
     // Program to check whether a number is prime or not
 
     public static void main(String[] args) {
-        
+
     }
 }
