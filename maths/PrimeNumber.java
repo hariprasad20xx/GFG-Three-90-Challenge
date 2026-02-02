224dac53cfeeb1aaed9620ba2e181e047a39921a
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-02 21:40:13 +0530

    Create Scanner object

--- maths/PrimeNumber.java
+++ maths/PrimeNumber.java
@@ -4,6 +4,8 @@ public class PrimeNumber {
     // Program to check whether a number is prime or not
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        scanner.close();
     }
 }
