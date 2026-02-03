b9a81a57d9e859c495b65b847c59f545a09e2072
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-03 06:45:32 +0530

    Take user input

--- maths/PrimeNumber.java
+++ maths/PrimeNumber.java
@@ -6,6 +6,9 @@ public class PrimeNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter a positive integer: ");
+        int n = scanner.nextInt();
+        
         scanner.close();
     }
 }
