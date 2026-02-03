06e5871768ce07e3978b65fd6843612c2a7062f5
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-03 06:46:46 +0530

    Add method signature for isPrime method

--- maths/PrimeNumber.java
+++ maths/PrimeNumber.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 public class PrimeNumber {
     // Program to check whether a number is prime or not
-
+    public static boolean isPrime(int n) {
+        return true;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a positive integer: ");
         int n = scanner.nextInt();
-        
+
         scanner.close();
     }
 }
