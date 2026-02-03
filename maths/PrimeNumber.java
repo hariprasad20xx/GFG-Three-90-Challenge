d033fceca3b44fd288cdd8f8559a2349e4477f4d
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-03 06:50:32 +0530

    Implement algorithm to check for prime number

--- maths/PrimeNumber.java
+++ maths/PrimeNumber.java
@@ -6,6 +6,12 @@ public class PrimeNumber {
         if (n <= 1) {
             return false;
         }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
         
         return true;
     }
