bbbdc47a113d43913a7196dcd79dad4498d4c3f0
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-05 21:51:32 +0530

    Add method signature for printNumbers method

--- recursion/PrintOneToN.java
+++ recursion/PrintOneToN.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 
 public class PrintOneToN {
     // Program to print 1 to N using recursion
-
+    public static void printNumbers(int N) {
+        
+    }
     public static void main(String[] agrs) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter the value of N: ");
         int n = scanner.nextInt();
-        
+
         scanner.close();
     }
 }
