d2ea8f75155d82b8ca48ba6847cf203e654b06c7
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-06 20:44:25 +0530

    Create Scanner object

--- recursion/PrintNToOne.java
+++ recursion/PrintNToOne.java
@@ -6,6 +6,8 @@ public class PrintNToOne {
     // Program to print N to one using recursion
 
     public static void main(String[] args) {
-    
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
     }
 }
