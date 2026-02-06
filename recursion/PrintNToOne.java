18b53e0000aadc2e807f5d78e234a23d855db0cf
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-06 20:43:17 +0530

    Import Scanner class

--- recursion/PrintNToOne.java
+++ recursion/PrintNToOne.java
@@ -1,9 +1,11 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class PrintNToOne {
     // Program to print N to one using recursion
 
     public static void main(String[] args) {
-        
+    
     }
 }
