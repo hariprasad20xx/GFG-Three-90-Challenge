ad5f2b5e12002dda287c45509b62233970e256ed
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-07 22:00:37 +0530

    Implement recursive logic to print numbers

--- recursion/PrintNToOne.java
+++ recursion/PrintNToOne.java
@@ -5,15 +5,21 @@ import java.util.Scanner;
 public class PrintNToOne {
     // Program to print N to one using recursion
     public static void printNumbers(int n) {
+        if (n == 0) {
+            return;
+        }
 
+        System.out.println(n);
+
+        printNumbers(n - 1);
     }
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter the value of N: ");
         int n = scanner.nextInt();
-
+        
         scanner.close();
     }
 }
