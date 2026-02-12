43823aff6abc00ad4c02bbd694cac88121afe9de
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-12 21:22:19 +0530

    Create Scanner object

--- searching/BinarySearch.java
+++ searching/BinarySearch.java
@@ -6,6 +6,8 @@ public class BinarySearch {
     // Program to implement binary search
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        scanner.close();
     }
 }
