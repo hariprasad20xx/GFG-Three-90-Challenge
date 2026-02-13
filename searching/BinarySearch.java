19e0ca3fc41f40a7feaaebbed679ecba84b1a59b
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-13 21:54:18 +0530

    Implement binary search algorithm

--- searching/BinarySearch.java
+++ searching/BinarySearch.java
@@ -6,8 +6,23 @@ public class BinarySearch {
     // Program to implement binary search
 
     static int binarySearch(int[] arr, int k) {
+        int low = 0;
+        int high = arr.length - 1;
+        int mid = (low + high) / 2;
+
+        while (low <= high) {
+            if (k == arr[mid]) {
+                return mid;
+            } else if (k < arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
         return -1;
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -23,7 +38,6 @@ public class BinarySearch {
 
         System.out.print("Enter the element to search in the array: ");
         int key = scanner.nextInt();
-
         
         scanner.close();
     }
