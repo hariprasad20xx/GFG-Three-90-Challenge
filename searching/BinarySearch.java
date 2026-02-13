a771745ad6bfa4d833398727020e107d357c045a
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-13 21:27:13 +0530

    Create array of size n

--- searching/BinarySearch.java
+++ searching/BinarySearch.java
@@ -11,6 +11,9 @@ public class BinarySearch {
         System.out.print("Enter the size of the array: ");
         int n = scanner.nextInt();
 
+        int[] arr = new int[n];
+
+
         scanner.close();
     }
 }
