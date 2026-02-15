eddcb9850163475b5b44c3a49ecf4fda6b8394c9
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-15 21:10:22 +0530

    Input array elements from user

--- arrays/LargestElement.java
+++ arrays/LargestElement.java
@@ -10,6 +10,13 @@ public class LargestElement {
         
         System.out.println("Enter the size of the array: ");
         int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.print("Enter " + n + " elements for the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
         scanner.close();
     }
 }
