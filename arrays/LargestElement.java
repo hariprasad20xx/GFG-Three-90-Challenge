b20bdf491b8b3b9611443d69517a6eb47c29923c
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-15 21:25:40 +0530

    Refactor code

--- arrays/LargestElement.java
+++ arrays/LargestElement.java
@@ -19,7 +19,7 @@ public class LargestElement {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Enter the size of the array: ");
+        System.out.print("Enter the size of the array: ");
         int n = scanner.nextInt();
 
         int[] arr = new int[n];
