d362cbb6efe950631f8732e2dc664255e83774a5
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-16 20:49:56 +0530

    Add user input for array elements

--- arrays/SecondLargest.java
+++ arrays/SecondLargest.java
@@ -11,6 +11,13 @@ public class SecondLargest {
         System.out.print("Enter the size of the array: ");
         int n = scanner.nextInt();
 
+        int[] arr = new int[n];
+
+        System.out.print("Enter " + n + " elements for the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
         scanner.close();
     }
 }
