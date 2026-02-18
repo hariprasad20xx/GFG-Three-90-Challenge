19636ab0af2ecdd7205489983698aaef6b7e1110
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-18 20:41:28 +0530

    Create Scanner object

--- arrays/ReverseArray.java
+++ arrays/ReverseArray.java
@@ -6,6 +6,8 @@ public class ReverseArray {
     // Program to reverse an array
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        scanner.close();
     }
 }
