84aadef47270a4dc4858f8bcb64e565bc37a5d74
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-18 20:39:52 +0530

    Import Scanner class

--- arrays/ReverseArray.java
+++ arrays/ReverseArray.java
@@ -1,9 +1,11 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class ReverseArray {
     // Program to reverse an array
 
     public static void main(String[] args) {
-        
+
     }
 }
