e8298bc9af197c2958145b55dbd3f86795399bcb
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-19 17:08:07 +0530

    Add result output in main

--- arrays/ReverseArray.java
+++ arrays/ReverseArray.java
@@ -31,6 +31,14 @@ public class ReverseArray {
             arr[i] = scanner.nextInt();
         }
 
+        reverseArray(arr);
+
+        System.out.print("The reversed array is: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        
         scanner.close();
     }
 }
