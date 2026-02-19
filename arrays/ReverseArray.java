530db040e648832e77ce7f26536605ea17d78845
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-19 17:00:40 +0530

    Add method signature for reverseArray method

--- arrays/ReverseArray.java
+++ arrays/ReverseArray.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class ReverseArray {
     // Program to reverse an array
+    static void reverseArray(int[] arr) {
+        
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -12,7 +15,7 @@ public class ReverseArray {
         int n = scanner.nextInt();
 
         int[] arr = new int[n];
-        
+
         System.out.print("Enter " + n + " elements for the array: ");
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
