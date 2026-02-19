b71e9bdd97b34a5e862ab02a9f131c5b1c01c81d
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-19 17:03:48 +0530

    Implement logic to reverse array

--- arrays/ReverseArray.java
+++ arrays/ReverseArray.java
@@ -5,7 +5,17 @@ import java.util.Scanner;
 public class ReverseArray {
     // Program to reverse an array
     static void reverseArray(int[] arr) {
-        
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low < high) {
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+
+            low++; 
+            high--;
+        }
     }
 
     public static void main(String[] args) {
