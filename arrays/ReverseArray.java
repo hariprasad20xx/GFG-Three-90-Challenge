06d8627e21128bfbb32ef35d7385e04707a23fa3
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-19 16:59:26 +0530

    Add user input for array elements

--- arrays/ReverseArray.java
+++ arrays/ReverseArray.java
@@ -10,7 +10,14 @@ public class ReverseArray {
 
         System.out.print("Enter the size of the array: ");
         int n = scanner.nextInt();
+
+        int[] arr = new int[n];
         
+        System.out.print("Enter " + n + " elements for the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
         scanner.close();
     }
 }
