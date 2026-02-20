4d879326b737da841b3e6f8f095d127b89d42aae
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-20 20:51:20 +0530

    Create Scanner object

--- recursion/Fibonacci.java
+++ recursion/Fibonacci.java
@@ -6,6 +6,8 @@ public class Fibonacci {
     // Program to find Nth fibonacci number
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
     }
 }
