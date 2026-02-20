cffe54761ec605ab9a86d0e7c95785b52020ee4f
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-20 20:52:58 +0530

    Add method signature for fibonacci method

--- recursion/Fibonacci.java
+++ recursion/Fibonacci.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Fibonacci {
     // Program to find Nth fibonacci number
 
+    static int fibonacci(int n) {
+        return 0;
+    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
