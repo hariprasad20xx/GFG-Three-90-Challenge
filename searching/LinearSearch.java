4eede9b4dbbcce5efcf4331da60c57bcdc6c0384
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-21 20:56:25 +0530

    Create Scanner object

--- searching/LinearSearch.java
+++ searching/LinearSearch.java
@@ -6,6 +6,8 @@ public class LinearSearch {
     // Program to search an element in array using Linear Search
 
     public static void main(String[] agrs) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
     }
 }
