3183464d9b84712c888928beff74fb25798321d1
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-22 20:59:36 +0530

    Add user input for array size

--- searching/LinearSearch.java
+++ searching/LinearSearch.java
@@ -8,6 +8,9 @@ public class LinearSearch {
     public static void main(String[] agrs) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+        
         scanner.close();
     }
 }
