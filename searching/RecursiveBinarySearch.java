7d660afb7620b2d9463a10c6a6c1fea2a482845a
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-23 18:42:51 +0530

    Import Scanner class

--- searching/RecursiveBinarySearch.java
+++ searching/RecursiveBinarySearch.java
@@ -1,9 +1,11 @@
 package searching;
 
+import java.util.Scanner;
+
 public class RecursiveBinarySearch {
     // Program to find element in an array using binary search
 
     public static void main(String[] agrs) {
-        
+
     }
 }
