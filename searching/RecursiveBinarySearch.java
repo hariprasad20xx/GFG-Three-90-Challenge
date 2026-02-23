25bc0e7bc8de3327ccb37e4561d360d0c8185a67
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-23 18:42:27 +0530

    Add main method

--- searching/RecursiveBinarySearch.java
+++ searching/RecursiveBinarySearch.java
@@ -1,5 +1,9 @@
 package searching;
 
 public class RecursiveBinarySearch {
+    // Program to find element in an array using binary search
 
+    public static void main(String[] agrs) {
+        
+    }
 }
