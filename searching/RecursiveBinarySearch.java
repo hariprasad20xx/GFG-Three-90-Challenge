cccd6c62e920c07622d1658926287783dbc128d7
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-23 18:43:40 +0530

    Create Scanner object

--- searching/RecursiveBinarySearch.java
+++ searching/RecursiveBinarySearch.java
@@ -6,6 +6,8 @@ public class RecursiveBinarySearch {
     // Program to find element in an array using binary search
 
     public static void main(String[] agrs) {
+        Scanner scanner = new Scanner(System.in);
 
+        scanner.close();
     }
 }
