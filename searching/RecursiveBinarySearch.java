ddfd265587867cefe333e76abc2474430e9ee3ef
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-24 22:50:48 +0530

    Add user input for array size

--- searching/RecursiveBinarySearch.java
+++ searching/RecursiveBinarySearch.java
@@ -8,6 +8,9 @@ public class RecursiveBinarySearch {
     public static void main(String[] agrs) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+
         scanner.close();
     }
 }
