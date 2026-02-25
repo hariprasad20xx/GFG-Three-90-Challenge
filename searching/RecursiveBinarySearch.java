fc9a6b5df5b5b454e55b8d17d41f14198225e4b5
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-25 22:51:08 +0530

    Implement recursive binary search algorithm

--- searching/RecursiveBinarySearch.java
+++ searching/RecursiveBinarySearch.java
@@ -5,7 +5,19 @@ import java.util.Scanner;
 public class RecursiveBinarySearch {
     // Program to find element in an array using binary search
 
-    static int binarySearch(int[] arr, int l, int r, int x) {
+    static int binarySearch(int[] arr, int l, int r, int k) {
+
+        if (r >= l) {
+            int mid = l + (r - l) / 2;
+
+            if (arr[mid] == k) 
+                return mid;
+
+            if (arr[mid] > k) 
+                return binarySearch(arr, l, mid - 1, k);
+
+            return binarySearch(arr, mid + 1, r, k);
+        }
         return -1;
     }
     
