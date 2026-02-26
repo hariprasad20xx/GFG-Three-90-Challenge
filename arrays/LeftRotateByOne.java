f0532a2808d0fc7145b60ee364b1cf8a9c5a5418
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-26 07:20:17 +0530

    Import Scanner class

--- arrays/LeftRotateByOne.java
+++ arrays/LeftRotateByOne.java
@@ -1,9 +1,11 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class LeftRotateByOne {
     // Program to left rotate an array by one place
 
     public static void main(String[] args) {
-        
+
     }
 }
