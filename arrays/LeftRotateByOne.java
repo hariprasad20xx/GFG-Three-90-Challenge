6084d1569c63bd03d7a6eb59de985ed463f39ffc
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-26 07:20:55 +0530

    Create Scanner object

--- arrays/LeftRotateByOne.java
+++ arrays/LeftRotateByOne.java
@@ -6,6 +6,8 @@ public class LeftRotateByOne {
     // Program to left rotate an array by one place
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        scanner.close();
     }
 }
