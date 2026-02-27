f5300535b9cb864ece80a9c854aed902a1f500c2
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-27 22:26:32 +0530

    Refactor code

--- arrays/LeftRotateByOne.java
+++ arrays/LeftRotateByOne.java
@@ -32,9 +32,10 @@ public class LeftRotateByOne {
 
         System.out.print("Rotated array: ");
         for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + "");
+            System.out.print(arr[i] + " ");
         }
-        
+        System.out.println();
+
         scanner.close();
     }
 }
