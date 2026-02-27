ac016adff63873336f9465ad44c5797d301f0b21
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-27 22:11:22 +0530

    Implement algorithm to rotate array

--- arrays/LeftRotateByOne.java
+++ arrays/LeftRotateByOne.java
@@ -6,9 +6,15 @@ public class LeftRotateByOne {
     // Program to left rotate an array by one place
 
     static void leftRotate(int[] arr) {
+        int temp = arr[0];
 
+        for (int i = 1; i < arr.length; i++) {
+            arr[i - 1] = arr[i];
+        }
+
+        arr[arr.length - 1] = temp;
     }
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
