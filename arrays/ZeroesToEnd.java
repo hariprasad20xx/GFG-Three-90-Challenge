dd16a254229063816f8d4e19fadaac1abefb4a30
Hariprasad <hariprasadjnv963@gmail.com>
2026-03-03 20:30:59 +0530

    Create Scanner object

--- arrays/ZeroesToEnd.java
+++ arrays/ZeroesToEnd.java
@@ -6,6 +6,8 @@ public class ZeroesToEnd {
     // Program to move zeroes in an array to end
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        scanner.close();
     }
 }
