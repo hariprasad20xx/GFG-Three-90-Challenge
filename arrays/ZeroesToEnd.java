7d781e6e8bc98cff0048b84a99824a4a0a7d58e5
Hariprasad <hariprasadjnv963@gmail.com>
2026-03-05 12:01:17 +0530

    Implement algorithm to move zeroes to end

--- arrays/ZeroesToEnd.java
+++ arrays/ZeroesToEnd.java
@@ -6,7 +6,15 @@ public class ZeroesToEnd {
     // Program to move zeroes in an array to end
     
     static void moveZeroesToEnd(int[] arr) {
+        int count = 0;
 
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[count];
+                arr[count] = temp;
+            }
+        }
     }
 
     public static void main(String[] args) {
