25f5e675e0f7e2d1526d6fe9d7f6b7c6b73be2c8
Hariprasad <hariprasadjnv963@gmail.com>
2026-03-06 21:20:45 +0530

    Add user input for input string

--- arrays/BettingGame.java
+++ arrays/BettingGame.java
@@ -7,6 +7,9 @@ public class BettingGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter a string containing only 'W' and 'L' (without space): ");
+        String gameString = scanner.next();
+
         scanner.close();
     }   
 }
