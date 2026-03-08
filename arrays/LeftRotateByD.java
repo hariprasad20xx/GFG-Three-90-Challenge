59fcb1be209c916de355500998b607d5c42abe7d
Hariprasad <hariprasadjnv963@gmail.com>
2026-03-08 17:24:37 +0530

    Create Scanner object

--- arrays/LeftRotateByD.java
+++ arrays/LeftRotateByD.java
@@ -6,6 +6,8 @@ public class LeftRotateByD {
     // Program to rotate an array by D places
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        scanner.close();
     }
 }
