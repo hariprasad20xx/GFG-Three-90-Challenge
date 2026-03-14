ac535a1a10ec26dd83ab7b7709ab714884f4569a
Hariprasad <hariprasadjnv963@gmail.com>
2026-03-14 22:10:45 +0530

    Add user input for array size

--- arrays/LeftRotateByD.java
+++ arrays/LeftRotateByD.java
@@ -8,6 +8,9 @@ public class LeftRotateByD {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+
         scanner.close();
     }
 }
