5931cc859ee9ad4660b32887d6f260666e99743f
Hariprasad <hariprasadjnv963@gmail.com>
2026-03-15 22:32:08 +0530

    Implement logic to reverse array

--- arrays/LeftRotateByD.java
+++ arrays/LeftRotateByD.java
@@ -9,7 +9,14 @@ public class LeftRotateByD {
     }
 
     static void reverse(int[] arr, int low, int high) {
-        
+        while (low < high) {
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+
+            low++;
+            high--;
+        }
     }
 
     public static void main(String[] args) {
