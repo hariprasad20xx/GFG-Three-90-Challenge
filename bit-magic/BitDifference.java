f69ea136dbfe43e956d04317a184564984b883c2
Hariprasad <hariprasadjnv963@gmail.com>
2026-03-18 21:07:00 +0530

    Create Scanner object

--- bit-magic/BitDifference.java
+++ bit-magic/BitDifference.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class BitDifference {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        scanner.close();
     }
 }
