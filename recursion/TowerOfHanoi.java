de37f98715d0af155edf3330d1d5e6fd78702283
Hariprasad <hariprasadjnv963@gmail.com>
2026-03-21 23:02:14 +0530

    Create Scanner object

--- recursion/TowerOfHanoi.java
+++ recursion/TowerOfHanoi.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class TowerOfHanoi {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        scanner.close();
     }
 
 }
