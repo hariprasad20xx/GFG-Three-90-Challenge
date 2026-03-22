f1283ad615c6d4c88907f8e4d3dfb7ae57e04716
Hariprasad <hariprasadjnv963@gmail.com>
2026-03-22 21:59:59 +0530

    Add base case in towerOfHanoi

--- recursion/TowerOfHanoi.java
+++ recursion/TowerOfHanoi.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class TowerOfHanoi {
 
     static void towerOfHanoi(int n, char fromRod, char toRod, char auxRod) {
-
+        if (n == 0) return;
     }
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
