20ce6cb367813320893114e893fbf1ff74337865
Hariprasad <hariprasadjnv963@gmail.com>
2026-03-22 21:58:22 +0530

    Add method signature for towerOfHanoi

--- recursion/TowerOfHanoi.java
+++ recursion/TowerOfHanoi.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class TowerOfHanoi {
 
+    static void towerOfHanoi(int n, char fromRod, char toRod, char auxRod) {
+
+    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
