ee3d972391817a55a17b9e553a462cbc67249560
Hariprasad <hariprasadjnv963@gmail.com>
2026-03-22 22:02:42 +0530

    Add recursive logic in towerOfHanoi

--- recursion/TowerOfHanoi.java
+++ recursion/TowerOfHanoi.java
@@ -6,6 +6,12 @@ public class TowerOfHanoi {
 
     static void towerOfHanoi(int n, char fromRod, char toRod, char auxRod) {
         if (n == 0) return;
+
+        towerOfHanoi(n - 1, fromRod, auxRod, toRod);
+		System.out.println("Move disk " + n + " from rod "
+						+ fromRod + " to rod "
+						+ toRod);
+		towerOfHanoi(n - 1, auxRod, toRod, fromRod);
     }
 
     public static void main(String[] args) {
