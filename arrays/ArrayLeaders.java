7a461601820c85f3704fec6e5b6ad9efe83cd188
Hariprasad <hariprasadjnv963@gmail.com>
2026-03-28 20:59:04 +0530

    Import ArrayList class

--- arrays/ArrayLeaders.java
+++ arrays/ArrayLeaders.java
@@ -1,9 +1,11 @@
 package arrays;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ArrayLeaders {
- 
+    
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
