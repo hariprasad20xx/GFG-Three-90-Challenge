5d1ca07890da893308fbf9fb5b7a2ca8f77011a3
Hariprasad <hariprasadjnv963@gmail.com>
2026-04-02 22:35:56 +0530

    Implement logic to find Square root

--- searching/SquareRoot.java
+++ searching/SquareRoot.java
@@ -9,7 +9,13 @@ public class SquareRoot {
         if (n == 0 || n == 1)
 			return n;
 
-        return n;
+        int i = 1, result = 1;
+
+		while (result <= n) {
+			i++;
+			result = i * i;
+		}
+		return i - 1;
     }
 
     public static void main(String[] args) {
