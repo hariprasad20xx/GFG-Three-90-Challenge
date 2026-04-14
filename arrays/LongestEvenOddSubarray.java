7d080e14d5733a481af1b8657a712126e1d0f880
Hariprasad <hariprasadjnv963@gmail.com>
2026-04-14 22:39:00 +0530

    Create array of given size

--- arrays/LongestEvenOddSubarray.java
+++ arrays/LongestEvenOddSubarray.java
@@ -10,6 +10,8 @@ public class LongestEvenOddSubarray {
         System.out.print("Enter the size of the array: ");
         int n = scanner.nextInt();
 
+        int[] arr = new int[n];
+
         scanner.close();
     }  
 }
