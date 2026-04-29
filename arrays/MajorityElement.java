d05ce59d63eaf373bd37e3b85b087e8f314dcaba
Hariprasad <hariprasadjnv963@gmail.com>
2026-04-29 21:53:11 +0530

    Implement findCandidate method

--- arrays/MajorityElement.java
+++ arrays/MajorityElement.java
@@ -15,7 +15,20 @@ public class MajorityElement {
     
 
     static int findCandidate(int[] a, int size) {
-
+        
+        int maj_index = 0, count = 1;
+		int i;
+		for (i = 1; i < size; i++) {
+			if (a[maj_index] == a[i])
+				count++;
+			else
+				count--;
+			if (count == 0) {
+				maj_index = i;
+				count = 1;
+			}
+		}
+		return a[maj_index];
     }
 
     static boolean isMajority(int a[], int size, int cand) {
