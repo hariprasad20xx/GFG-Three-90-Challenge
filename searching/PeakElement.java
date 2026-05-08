1b0eb17248a248d0da20dcd0827ebdf240cdd19d
Hariprasad <hariprasadjnv963@gmail.com>
2026-05-08 16:08:40 +0530

    Add user input for array size

--- searching/PeakElement.java
+++ searching/PeakElement.java
@@ -3,9 +3,12 @@ package searching;
 import java.util.Scanner;
 
 public class PeakElement {
-
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
 
         scanner.close();
     }
