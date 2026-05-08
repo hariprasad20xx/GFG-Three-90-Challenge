0de65478620d5d7f30c57ad33333a89766fce83c
Hariprasad <hariprasadjnv963@gmail.com>
2026-05-08 16:09:55 +0530

    Create array of given size

--- searching/PeakElement.java
+++ searching/PeakElement.java
@@ -10,6 +10,8 @@ public class PeakElement {
         System.out.print("Enter the size of the array: ");
         int n = scanner.nextInt();
 
+        int[] arr = new int[n];
+
         scanner.close();
     }
 }
