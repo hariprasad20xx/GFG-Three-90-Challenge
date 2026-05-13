dc815716d708afa27bfef66400a92e3612c8be39
Hariprasad <hariprasadjnv963@gmail.com>
2026-05-13 22:05:36 +0530

    Implement firstOcc method

--- searching/CountOccurence.java
+++ searching/CountOccurence.java
@@ -5,6 +5,30 @@ import java.util.Scanner;
 public class CountOccurence {
 
     static int firstOcc(int[] arr, int n, int x) {
+
+        int low = 0, high = n - 1;
+
+		while(low <= high)
+		{
+			int mid = (low + high) / 2;
+
+			if(x > arr[mid])
+				low = mid + 1;
+
+			else if(x < arr[mid])
+				high = mid - 1;
+
+			else
+			{
+				if(mid == 0 || arr[mid - 1] != arr[mid])
+					return mid;
+
+				else
+					high = mid - 1;
+			}
+
+		}
+        
         return -1;
     }
 
