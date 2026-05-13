fd2b6eb9cb5f56653f67cebe47be94b3ea54f989
Hariprasad <hariprasadjnv963@gmail.com>
2026-05-13 22:09:04 +0530

    Implement lastOcc method

--- searching/CountOccurence.java
+++ searching/CountOccurence.java
@@ -28,11 +28,35 @@ public class CountOccurence {
 			}
 
 		}
-        
+
         return -1;
     }
 
     static int lastOcc(int[] arr, int n, int x) {
+
+        int low = 0, high = n - 1;
+
+		while(low <= high)
+		{
+			int mid = (low + high) / 2;
+
+			if(x > arr[mid])
+				low = mid + 1;
+
+			else if(x < arr[mid])
+				high = mid - 1;
+
+			else
+			{
+				if(mid == n - 1 || arr[mid + 1] != arr[mid])
+					return mid;
+
+				else
+					low = mid + 1;
+			}
+
+		}
+        
         return -1;
     }
 
