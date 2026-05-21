1cdada3696d5066b4c9fd4c5f293cfa146ff1f41
Hariprasad <hariprasadjnv963@gmail.com>
2026-05-21 22:28:38 +0530

    Implement countOnes method

--- searching/CountOnes.java
+++ searching/CountOnes.java
@@ -5,7 +5,25 @@ import java.util.Scanner;
 public class CountOnes {
 
     static int countOnes(int[] arr, int n) {
-        return 0;
+        
+        int low = 0, high = n - 1;
+
+		while(low <= high)
+		{
+			int mid = (low + high) / 2;
+
+			if(arr[mid] == 0)
+				low = mid + 1;
+			else
+			{
+				if(mid == 0 || arr[mid - 1] == 0)
+					return (n - mid);
+				else 
+					high = mid -1;
+			}
+		}
+
+		return 0;
     }
     
     public static void main(String[] args) {
