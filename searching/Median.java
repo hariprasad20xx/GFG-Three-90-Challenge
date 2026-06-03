225300011242db3a863c267dad14757ccb6b75f1
Hariprasad <hariprasadjnv963@gmail.com>
2026-06-03 21:39:40 +0530

    Add user input for array size

--- searching/Median.java
+++ searching/Median.java
@@ -7,6 +7,9 @@ public class Median {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the size of the first array: ");
+        int n1 = scanner.nextInt();
+
         scanner.close();
     }
 }
