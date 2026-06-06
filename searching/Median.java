12ea77fa5b65733ebe69a0794704d1ffb2b8ec29
Hariprasad <hariprasadjnv963@gmail.com>
2026-06-06 22:03:00 +0530

    Implement logic to find median

--- searching/Median.java
+++ searching/Median.java
@@ -10,6 +10,33 @@ public class Median {
         if (n > m) {
             return median(B, A);
         }
+
+        int start = 0;
+		int end = n;
+		int realMidInMergedArray = (n + m + 1) / 2;
+
+        while (start <= end) {
+			int mid = (start + end) / 2;
+			int leftAsize = mid;
+			int leftBsize = realMidInMergedArray - mid;
+			int leftA = (leftAsize > 0) ? A[leftAsize - 1] : Integer.MIN_VALUE;										
+			int leftB = (leftBsize > 0) ? B[leftBsize - 1] : Integer.MIN_VALUE;
+			int rightA = (leftAsize < n) ? A[leftAsize] : Integer.MAX_VALUE;
+			int rightB = (leftBsize < m) ? B[leftBsize] : Integer.MAX_VALUE;
+
+			// if correct partition is done
+			if (leftA <= rightB && leftB <= rightA) {
+				if ((m + n) % 2 == 0) {
+					return (Math.max(leftA, leftB) + Math.min(rightA, rightB)) / 2.0;
+                }
+				return Math.max(leftA, leftB);
+			}
+			else if (leftA > rightB) {
+				end = mid - 1;
+			}
+			else
+				start = mid + 1;
+        }
         
         return 0.0;
     }
